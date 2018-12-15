54eea68ad426faa3e62bc630045700e16bb8c4d2
Microsoft <1564940789@qq.com>
2018-12-15 11:09:31 +0800

    Create start.sh

---
+++ start.sh
@@ -0,0 +1,15 @@
+apt update
+apt install -y openjdk-8-jdk
+apt install -y make
+apt install -y gcc
+apt install -y wget
+apt install -y git flex bison gperf build-essential libncurses5-dev:i386 
+apt install -y libx11-dev:i386 libreadline6-dev:i386 libgl1-mesa-dev g++-multilib 
+apt install -y tofrodos python-markdown libxml2-utils xsltproc zlib1g-dev:i386 
+apt install -y dpkg-dev libsdl1.2-dev libesd0-dev
+apt install -y git-core gnupg flex bison gperf build-essential  
+apt install -y zip curl zlib1g-dev gcc-multilib g++-multilib 
+apt install -y libc6-dev-i386 
+apt install -y lib32ncurses5-dev x11proto-core-dev libx11-dev 
+apt install -y lib32z-dev ccache
+apt install -y libgl1-mesa-dev libxml2-utils xsltproc unzip m4
