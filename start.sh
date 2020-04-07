7a1036bf412e8d422c714bdee48dd6959bc749fa
LLT <1564940789@qq.com>
2020-04-07 09:56:25 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -13,3 +13,18 @@ apt install -y libc6-dev-i386
 apt install -y lib32ncurses5-dev x11proto-core-dev libx11-dev 
 apt install -y lib32z-dev ccache
 apt install -y libgl1-mesa-dev libxml2-utils xsltproc unzip m4
+
+apt install libx11-dev:i386 libreadline6-dev:i386 libgl1-mesa-dev g++-multilib -y
+apt install -y git flex bison gperf build-essential libncurses5-dev:i386
+apt install dpkg-dev libsdl1.2-dev libesd0-dev
+apt install git-core gnupg flex bison gperf build-essential
+apt install zip curl zlib1g-dev gcc-multilib g++-multilib
+apt install libc6-dev-i386 -y
+apt install lib32ncurses5-dev x11proto-core-dev libx11-dev
+apt install libgl1-mesa-dev libxml2-utils xsltproc unzip m4
+apt install lib32z-dev ccache
+apt install make make-guile -y
+apt install tofrodos -y
+apt install python-markdown libxml2-utils xsltproc zlib1g-dev:i386
+
+apt install -y git-core gnupg flex bison gperf build-essential zip curl zlib1g-dev gcc-multilib g++-multilib libc6-dev-i386 lib32ncurses5-dev x11proto-core-dev libx11-dev lib32z-dev ccache libgl1-mesa-dev libxml2-utils xsltproc unzip
